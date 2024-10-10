e128d103cd2fd9dc8932f8a9fe22be7b405574f2
karu <32568363+karu-rress@users.noreply.github.com>
2024-10-10 09:21:17 +0900

    added cookie w/ base64

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,2 +1,2 @@
 const base64cookie = "WW91J3ZlIGZvdW5kIHRoaXMh";
-document.cookie = `cookie=${base64cookie}; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+document.cookie = `cookie=${atob(base64cookie)}; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
